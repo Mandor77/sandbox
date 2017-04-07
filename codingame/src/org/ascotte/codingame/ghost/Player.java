07add17b15675e448c66eb50f1d0a8a98a4746a6
Mandor77 <adrien.scotte@laposte.net>
2017-04-07 23:17:03 +0200

    codingame:ghost: Solve simple conflicts

--- codingame/src/org/ascotte/codingame/ghost/Player.java
+++ codingame/src/org/ascotte/codingame/ghost/Player.java
@@ -7,27 +7,121 @@ public class Player {
 class Game {
 	
 	Factory[] factories;
-	FactoryBuilder builder = new FactoryBuilder();
+	Troop[] troops;
+	Builder builder = new Builder();
 	
-	public Game (int nbFactories) {
+	public Game (int nbFactories, int nbTroops) {
 		this.factories = new Factory[nbFactories];
+		this.troops = new Troop[nbTroops];
 	}
 	
 	public void addFactory(int id, Owner owner, int production, int nbCyborgs) {
 		this.factories[id] = builder.createFactory(id, owner, production, nbCyborgs);
 	}
 	
+	public void addTroop(int id, Owner owner, int from, int to, int nbCyborgs, int remainingTurns) {
+		this.troops[id] = builder.createTroop(id, owner, from, to, nbCyborgs, remainingTurns);
+	}
+	
 	public Factory getFactory(int id) {
 		return factories[id];
 	}
 	
+	public Troop getTroop(int id) {
+		return troops[id];
+	}
+	
 	public void play(){
+		
+		// Move management
+		for (int id = 0; id < troops.length; id++) {
+			Troop troop = this.troops[id];
+			troop.move();
+		}
+		
+		// Production management
 		for (int id = 0; id < factories.length; id++) {
 			Factory factory = this.factories[id];
 			if (factory.getOwner() != Owner.NOBODY) {
 				factory.addProductionToNbCyborgs();
 			}
 		}
+		
+		// Battle management
+		for (int id = 0; id < troops.length; id++) {
+			Troop troop = this.troops[id];
+			if (troop.getRemainingTurns() == 0) {
+				Factory factory = this.factories[troop.getTo()];
+				if (troop.getOwner().equals(factory.getOwner())) {
+					factory.addCyborgs(troop.getNbCyborgs());
+				}
+				else {
+					factory.removeCyborgs(troop.getNbCyborgs());
+				}
+			}
+		}
+
+	}
+}
+
+class Troop {
+	int id;
+	Owner owner;
+	int from;
+	int to;
+	int nbCyborgs;
+	int remainingTurns;
+	
+	public Troop(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+	public int getFrom() {
+		return from;
+	}
+
+	public void setFrom(int from) {
+		this.from = from;
+	}
+
+	public int getTo() {
+		return to;
+	}
+
+	public void setTo(int to) {
+		this.to = to;
+	}
+
+	public int getNbCyborgs() {
+		return nbCyborgs;
+	}
+
+	public void setNbCyborgs(int nbCyborgs) {
+		this.nbCyborgs = nbCyborgs;
+	}
+
+	public int getRemainingTurns() {
+		return remainingTurns;
+	}
+
+	public void setRemainingTurns(int remainingTurns) {
+		this.remainingTurns = remainingTurns;
+	}
+	
+	public void move() {
+		this.remainingTurns -= 1;
 	}
 }
 
@@ -40,7 +134,7 @@ class Factory {
 	public Factory(int id) {
 		this.id = id;
 	}
-	
+
 	public int getId() {
 		return this.id;
 	}
@@ -72,9 +166,17 @@ class Factory {
 	public void addProductionToNbCyborgs() {
 		this.nbCyborgs += this.production;
 	}
+	
+	public void addCyborgs(int nbCyborgs) {
+		this.nbCyborgs += nbCyborgs;
+	}
+	
+	public void removeCyborgs(int nbCyborgs) {
+		this.nbCyborgs -= nbCyborgs;
+	}
 }
 
-class FactoryBuilder {
+class Builder {
 	
 	public Factory createFactory(int id, Owner owner, int production, int nbCyborgs) {
 		Factory factory = new Factory(id);
@@ -82,6 +184,16 @@ class FactoryBuilder {
 		factory.setOwner(owner);
 		factory.setProduction(production);
 		return factory;
+	}
+	
+	public Troop createTroop(int id, Owner owner, int from, int to, int nbCyborgs, int remainingTurns) {
+		Troop troop = new Troop(id);
+		troop.setOwner(owner);
+		troop.setFrom(from);
+		troop.setTo(to);
+		troop.setNbCyborgs(nbCyborgs);
+		troop.setRemainingTurns(remainingTurns);
+		return troop;
 	}
 }
 
--- codingame/src/org/ascotte/codingame/ghost/PlayerTest.java
+++ codingame/src/org/ascotte/codingame/ghost/PlayerTest.java
@@ -7,7 +7,7 @@ public class PlayerTest {
 
 	@Test
 	public void productionInOneFactoryOwnedByMeIncreaseCyborgNumber() {
-		Game game = new Game(1);
+		Game game = new Game(1, 0);
 		game.addFactory(0, Owner.PLAYER, 3, 2);
 		
 		game.play();
@@ -17,7 +17,7 @@ public class PlayerTest {
 	
 	@Test
 	public void productionInSeveralFactoriesOwnedByMeIncreaseCyborgNumber() {
-		Game game = new Game(2);
+		Game game = new Game(2, 0);
 		game.addFactory(0, Owner.PLAYER, 3, 2);
 		game.addFactory(1, Owner.PLAYER, 2, 2);
 		
@@ -29,7 +29,7 @@ public class PlayerTest {
 	
 	@Test
 	public void productionInOneFactoryOwnedByEnnemyIncreaseCyborgNumber() {
-		Game game = new Game(1);
+		Game game = new Game(1, 0);
 		game.addFactory(0, Owner.OPPONENT, 3, 2);
 		
 		game.play();
@@ -39,11 +39,61 @@ public class PlayerTest {
 	
 	@Test
 	public void productionInOneFactoryOwnedByNeutralIncreaseCyborgNumber() {
-		Game game = new Game(1);
+		Game game = new Game(1, 0);
 		game.addFactory(0, Owner.NOBODY, 3, 2);
 		
 		game.play();
 		
 		Assert.assertEquals("Production increase is not done on your factory", 2, game.getFactory(0).getNbCyborgs());
+	}
+	
+	@Test
+	public void movingTroopProgressOfOneInATurn() {
+		Game game = new Game(2, 1);
+		game.addFactory(0, Owner.PLAYER, 3, 2);
+		game.addFactory(1, Owner.OPPONENT, 3, 2);
+		game.addTroop(0, Owner.PLAYER, 0, 1, 1, 5);
+		
+		game.play();
+		
+		Assert.assertEquals("Troop move is not done in a game", 4, game.getTroop(0).getRemainingTurns());
+	}
+	
+	@Test
+	public void factoryIsAttackedByAEnnemyArrivedTroop() {
+		Game game = new Game(2, 1);
+		game.addFactory(0, Owner.PLAYER, 0, 2);
+		game.addFactory(1, Owner.OPPONENT, 0, 2);
+		game.addTroop(0, Owner.PLAYER, 0, 1, 1, 1);
+		
+		game.play();
+		
+		Assert.assertEquals("Troop does not attack when arrives", 1, game.getFactory(1).getNbCyborgs());
+	}
+	
+	@Test
+	public void factoryIsReinforcedByAnAmicalArrivedTroop() {
+		Game game = new Game(2, 1);
+		game.addFactory(0, Owner.PLAYER, 0, 2);
+		game.addFactory(1, Owner.OPPONENT, 0, 2);
+		game.addTroop(0, Owner.OPPONENT, 0, 1, 1, 1);
+		
+		game.play();
+		
+		Assert.assertEquals("Troop does not attack when arrives", 3, game.getFactory(1).getNbCyborgs());
+	}
+	
+	@Test
+	public void troopConflictAreResolvedBeforeAttackingFactory() {
+		Game game = new Game(3, 2);
+		game.addFactory(0, Owner.PLAYER, 0, 2);
+		game.addFactory(1, Owner.OPPONENT, 0, 2);
+		game.addFactory(2, Owner.PLAYER, 0, 2);
+		game.addTroop(0, Owner.OPPONENT, 0, 1, 1, 1);
+		game.addTroop(1, Owner.PLAYER, 2, 1, 1, 1);
+		
+		game.play();
+		
+		Assert.assertEquals("Conflicts are not resolved before impacting factory", 2, game.getFactory(1).getNbCyborgs());
 	}
 }
